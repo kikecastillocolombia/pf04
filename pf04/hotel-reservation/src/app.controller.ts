ba166373d48ce488284664f6276550ce4c0c55d4
Enrique Castillo <168988661+kikecastillocolombia@users.noreply.github.com>
2024-09-17 00:18:53 -0500

    Revert "commit"

---
+++ pf04/hotel-reservation/src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService, Person } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): Person {
+    return this.appService.getHello();
+  }
+}
